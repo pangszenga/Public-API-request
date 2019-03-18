cb46aa93dbab18225d626676e634e804ab577585
Priscilla Pang <Priscilla.pang@keystonelaw.co.uk>
2019-03-18 19:16:52 +0000

    Set up

--- public_api_request-v1/js/app.js
+++ public_api_request-v1/js/app.js
@@ -3,7 +3,63 @@ $(document).ready(function()
   const $search = $(".search-container");
   const $gallery = $("#gallery");
   let form = document.createElement("form");
-  form.innerHTML = 
-  //create form
-  console.log(form);
+  let card = document.createElement("div");
+  let modal = document.createElement("div");
+
+  //construct
+  form.innerHTML =
+  `<input type="search" id="search-input" class="search-input" placeholder="Search...">
+   <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">`;
+
+  card.innerHTML =
+  `<div class="card-img-container">
+   <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+   </div>
+   <div class="card-info-container">
+   <h3 id="name" class="card-name cap">first last</h3>
+   <p class="card-text">email</p>
+   <p class="card-text cap">city, state</p>
+   </div>`;
+
+  modal.innerHTML =
+  `<div class="modal">
+   <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+   <div class="modal-info-container">
+   <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+   <h3 id="name" class="modal-name cap">name</h3>
+   <p class="modal-text">email</p>
+   <p class="modal-text cap">city</p>
+   <hr>
+   <p class="modal-text">(555) 555-5555</p>
+   <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+   <p class="modal-text">Birthday: 10/21/2015</p>
+   </div>
+   </div>
+   <div class="modal-btn-container">
+   <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+   <button type="button" id="modal-next" class="modal-next btn">Next</button>
+   </div>`;
+   //remember to add class modal-container
+
+  //fetch
+  fetch('https://randomuser.me/api/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+  });
+
+
+  //append things
+  $search.append(form);
+  $gallery.append(card).first().addClass("card");
+
+
+
+
+
+
+
+
 });//documment load ends
