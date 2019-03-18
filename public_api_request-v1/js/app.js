600a7af27764d5a55886c4cb0ba0609c2a26c5e6
Priscilla Pang <Priscilla.pang@keystonelaw.co.uk>
2019-03-18 00:41:47 +0000

    JQuery added

--- public_api_request-v1/js/app.js
+++ public_api_request-v1/js/app.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function()
+{
+  const $search = $(".search-container");
+  const $gallery = $("#gallery");
+  let form = document.createElement("form");
+  form.innerHTML = 
+  //create form
+  console.log(form);
+});//documment load ends
